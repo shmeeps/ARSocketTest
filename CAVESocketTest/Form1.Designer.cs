1bdfc8a39828b76c12646b68460ebea6107afc2c
Shmeeps <killershmeeps@yahoo.com>
2012-03-23 10:41:15 -0500

    Test

---
+++ CAVESocketTest/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace CAVESocketTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.forwardButton = new System.Windows.Forms.Button();
+            this.leftButton = new System.Windows.Forms.Button();
+            this.backButton = new System.Windows.Forms.Button();
+            this.rightButton = new System.Windows.Forms.Button();
+            this.upButton = new System.Windows.Forms.Button();
+            this.downButton = new System.Windows.Forms.Button();
+            this.takeOffButton = new System.Windows.Forms.Button();
+            this.emergencyButton = new System.Windows.Forms.Button();
+            this.flatTrimButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.disconnectButton = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.connectedStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.landButton = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // forwardButton
+            // 
+            this.forwardButton.Location = new System.Drawing.Point(106, 12);
+            this.forwardButton.Name = "forwardButton";
+            this.forwardButton.Size = new System.Drawing.Size(75, 23);
+            this.forwardButton.TabIndex = 0;
+            this.forwardButton.Text = "Forward";
+            this.forwardButton.UseVisualStyleBackColor = true;
+            this.forwardButton.Click += new System.EventHandler(this.forwardButton_Click);
+            // 
+            // leftButton
+            // 
+            this.leftButton.Location = new System.Drawing.Point(12, 42);
+            this.leftButton.Name = "leftButton";
+            this.leftButton.Size = new System.Drawing.Size(75, 23);
+            this.leftButton.TabIndex = 1;
+            this.leftButton.Text = "Left";
+            this.leftButton.UseVisualStyleBackColor = true;
+            this.leftButton.Click += new System.EventHandler(this.leftButton_Click);
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(106, 42);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(75, 23);
+            this.backButton.TabIndex = 2;
+            this.backButton.Text = "Backward";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // rightButton
+            // 
+            this.rightButton.Location = new System.Drawing.Point(197, 42);
+            this.rightButton.Name = "rightButton";
+            this.rightButton.Size = new System.Drawing.Size(75, 23);
+            this.rightButton.TabIndex = 3;
+            this.rightButton.Text = "Right";
+            this.rightButton.UseVisualStyleBackColor = true;
+            this.rightButton.Click += new System.EventHandler(this.rightButton_Click);
+            // 
+            // upButton
+            // 
+            this.upButton.Location = new System.Drawing.Point(197, 71);
+            this.upButton.Name = "upButton";
+            this.upButton.Size = new System.Drawing.Size(75, 23);
+            this.upButton.TabIndex = 4;
+            this.upButton.Text = "Up";
+            this.upButton.UseVisualStyleBackColor = true;
+            this.upButton.Click += new System.EventHandler(this.upButton_Click);
+            // 
+            // downButton
+            // 
+            this.downButton.Location = new System.Drawing.Point(197, 100);
+            this.downButton.Name = "downButton";
+            this.downButton.Size = new System.Drawing.Size(75, 23);
+            this.downButton.TabIndex = 5;
+            this.downButton.Text = "Down";
+            this.downButton.UseVisualStyleBackColor = true;
+            this.downButton.Click += new System.EventHandler(this.downButton_Click);
+            // 
+            // takeOffButton
+            // 
+            this.takeOffButton.Location = new System.Drawing.Point(12, 71);
+            this.takeOffButton.Name = "takeOffButton";
+            this.takeOffButton.Size = new System.Drawing.Size(169, 23);
+            this.takeOffButton.TabIndex = 6;
+            this.takeOffButton.Text = "Take Off";
+            this.takeOffButton.UseVisualStyleBackColor = true;
+            this.takeOffButton.Click += new System.EventHandler(this.takeOffButton_Click);
+            // 
+            // emergencyButton
+            // 
+            this.emergencyButton.Location = new System.Drawing.Point(12, 129);
+            this.emergencyButton.Name = "emergencyButton";
+            this.emergencyButton.Size = new System.Drawing.Size(169, 121);
+            this.emergencyButton.TabIndex = 7;
+            this.emergencyButton.Text = "Emergency";
+            this.emergencyButton.UseVisualStyleBackColor = true;
+            this.emergencyButton.Click += new System.EventHandler(this.emergencyButton_Click);
+            // 
+            // flatTrimButton
+            // 
+            this.flatTrimButton.Location = new System.Drawing.Point(197, 129);
+            this.flatTrimButton.Name = "flatTrimButton";
+            this.flatTrimButton.Size = new System.Drawing.Size(75, 121);
+            this.flatTrimButton.TabIndex = 8;
+            this.flatTrimButton.Text = "Flat Trim";
+            this.flatTrimButton.UseVisualStyleBackColor = true;
+            this.flatTrimButton.Click += new System.EventHandler(this.flatTrimButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 256);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 224);
+            this.textBox1.TabIndex = 9;
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(12, 12);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(75, 23);
+            this.connectButton.TabIndex = 10;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // disconnectButton
+            // 
+            this.disconnectButton.Enabled = false;
+            this.disconnectButton.Location = new System.Drawing.Point(197, 12);
+            this.disconnectButton.Name = "disconnectButton";
+            this.disconnectButton.Size = new System.Drawing.Size(75, 23);
+            this.disconnectButton.TabIndex = 11;
+            this.disconnectButton.Text = "Disconnect";
+            this.disconnectButton.UseVisualStyleBackColor = true;
+            this.disconnectButton.Click += new System.EventHandler(this.disconnectButton_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectedStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 485);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(284, 22);
+            this.statusStrip1.TabIndex = 12;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // connectedStatus
+            // 
+            this.connectedStatus.Name = "connectedStatus";
+            this.connectedStatus.Size = new System.Drawing.Size(79, 17);
+            this.connectedStatus.Text = "Disconnected";
+            // 
+            // landButton
+            // 
+            this.landButton.Location = new System.Drawing.Point(12, 100);
+            this.landButton.Name = "landButton";
+            this.landButton.Size = new System.Drawing.Size(169, 23);
+            this.landButton.TabIndex = 13;
+            this.landButton.Text = "Land";
+            this.landButton.UseVisualStyleBackColor = true;
+            this.landButton.Click += new System.EventHandler(this.landButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 507);
+            this.Controls.Add(this.landButton);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.disconnectButton);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.flatTrimButton);
+            this.Controls.Add(this.emergencyButton);
+            this.Controls.Add(this.takeOffButton);
+            this.Controls.Add(this.downButton);
+            this.Controls.Add(this.upButton);
+            this.Controls.Add(this.rightButton);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.leftButton);
+            this.Controls.Add(this.forwardButton);
+            this.Name = "Form1";
+            this.Text = "CAVESocketTest";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button forwardButton;
+        private System.Windows.Forms.Button leftButton;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button rightButton;
+        private System.Windows.Forms.Button upButton;
+        private System.Windows.Forms.Button downButton;
+        private System.Windows.Forms.Button takeOffButton;
+        private System.Windows.Forms.Button emergencyButton;
+        private System.Windows.Forms.Button flatTrimButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Button disconnectButton;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel connectedStatus;
+        private System.Windows.Forms.Button landButton;
+    }
+}
+
---
+++ CAVESocketTest/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace CAVESocketTest
+{
+    public partial class Form1 : Form
+    {
+        byte[] m_dataBuffer = new byte [10];
+		IAsyncResult m_result;
+		public AsyncCallback m_pfnCallBack ;
+		public Socket m_clientSocket;
+
+        public String m_IPAddress = "127.0.0.1";
+        public String m_Port      = "8000";
+
+        public enum Commands
+        {
+            None = 0,
+            Forward = 1,
+            Backward = 2,
+            Left = 3,
+            Right = 4,
+            Up = 5,
+            Down = 6,
+            FlatTrim = 7,
+            TakeOff = 8,
+            Land = 9,
+            Emergency = 10,
+            StrafeL = 11,
+            StrafeR = 12,
+            Camera = 13,
+            Special = 14,
+            Hover = 15,
+            Calibrate = 16,
+            CalibrationComplete = 17,
+            ControlToPatient = 18,
+            ControlToSupervisor = 19,
+            CheckInToggle = 20,
+            SelectPatient = 21,
+            SavePatient = 22,
+            ViewLogs = 23,
+            ViewRecordings = 24,
+            StartSimulation = 25,
+            EndSimulation = 26,
+            PauseSimulation = 27,
+            Exit = 28
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        // Connect button
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                UpdateControls(false);
+                // Create the socket instance
+                m_clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                // Cet the remote IP address
+                IPAddress ip = IPAddress.Parse(m_IPAddress);
+                int iPortNo = System.Convert.ToInt16(m_Port);
+                // Create the end point 
+                IPEndPoint ipEnd = new IPEndPoint(ip, iPortNo);
+                // Connect to the remote host
+                m_clientSocket.Connect(ipEnd);
+                if (m_clientSocket.Connected)
+                {
+
+                    UpdateControls(true);
+                    //Wait for data asynchronously 
+                    WaitForData();
+                }
+            }
+            catch (SocketException se)
+            {
+                string str;
+                str = "\nConnection failed, is the server running?\n" + se.Message;
+                MessageBox.Show(str);
+                UpdateControls(false);
+            }		
+        }
+
+        // Disconnect
+        private void disconnectButton_Click(object sender, EventArgs e)
+        {
+            if (m_clientSocket != null)
+            {
+                m_clientSocket.Close();
+                m_clientSocket = null;
+            }
+        }
+
+        // ---------------------------------------------------------------- //
+        // -------------------------- MAIN CHUNK -------------------------- //
+        // ---------------------------------------------------------------- //
+
+        // Left
+        private void leftButton_Click(object sender, EventArgs e)
+        {
+            sendCommand(Commands.Left);
+        }
+
+        // Foward
+        private void forwardButton_Click(object sender, EventArgs e)
+        {
+            sendCommand(Commands.Forward);
+        }
+
+        // Back
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            sendCommand(Commands.Backward);
+        }
+
+        // Right
+        private void rightButton_Click(object sender, EventArgs e)
+        {
+            sendCommand(Commands.Right);
+        }
+
+        // Up
+        private void upButton_Click(object sender, EventArgs e)
+        {
+            sendCommand(Commands.Up);
+        }
+
+        // Down
+        private void downButton_Click(object sender, EventArgs e)
+        {
+            sendCommand(Commands.Down);
+        }
+
+        // Send Flat Trim
+        private void flatTrimButton_Click(object sender, EventArgs e)
+        {
+            sendCommand(Commands.FlatTrim);
+        }
+
+        // Take Off
+        private void takeOffButton_Click(object sender, EventArgs e)
+        {
+            sendCommand(Commands.TakeOff);
+        }
+
+        // Land
+        private void landButton_Click(object sender, EventArgs e)
+        {
+            sendCommand(Commands.Land);
+        }
+
+        // Emergency
+        private void emergencyButton_Click(object sender, EventArgs e)
+        {
+            sendCommand(Commands.Emergency);
+        }
+
+        // Sends a command
+        private void sendCommand(Commands cmd)
+        {
+            try
+            {
+                byte[] byData = System.Text.Encoding.ASCII.GetBytes(((int)cmd).ToString());
+                if (m_clientSocket != null)
+                {
+                    m_clientSocket.Send(byData);
+                }
+            }
+            catch (SocketException se)
+            {
+                MessageBox.Show(se.Message);
+            }
+
+            Thread.Sleep(500);
+            
+            try
+            {
+                byte[] byData = System.Text.Encoding.ASCII.GetBytes(((int)Commands.None).ToString());
+                if (m_clientSocket != null)
+                {
+                    m_clientSocket.Send(byData);
+                }
+            }
+            catch (SocketException se)
+            {
+                MessageBox.Show(se.Message);
+            }
+        }
+
+        // ---------------------------------------------------------------- //
+        // -------------------------- END  CHUNK -------------------------- //
+        // ---------------------------------------------------------------- //
+
+        // Wait for incoming data if needed (unused)
+        public void WaitForData()
+        {
+            try
+            {
+                if (m_pfnCallBack == null)
+                {
+                    m_pfnCallBack = new AsyncCallback(OnDataReceived);
+                }
+                SocketPacket theSocPkt = new SocketPacket();
+                theSocPkt.thisSocket = m_clientSocket;
+                // Start listening to the data asynchronously
+                m_result = m_clientSocket.BeginReceive(theSocPkt.dataBuffer,
+                                                        0, theSocPkt.dataBuffer.Length,
+                                                        SocketFlags.None,
+                                                        m_pfnCallBack,
+                                                        theSocPkt);
+            }
+            catch (SocketException se)
+            {
+                MessageBox.Show(se.Message);
+            }
+        }
+
+        // Event for data being received (unused)
+        public void OnDataReceived(IAsyncResult asyn)
+        {
+            try
+            {
+                SocketPacket theSockId = (SocketPacket)asyn.AsyncState;
+                int iRx = theSockId.thisSocket.EndReceive(asyn);
+                char[] chars = new char[iRx + 1];
+                System.Text.Decoder d = System.Text.Encoding.UTF8.GetDecoder();
+                int charLen = d.GetChars(theSockId.dataBuffer, 0, iRx, chars, 0);
+                System.String szData = new System.String(chars);
+                // richTextRxMessage.Text = richTextRxMessage.Text + szData;
+                WaitForData();
+            }
+            catch (ObjectDisposedException)
+            {
+                System.Diagnostics.Debugger.Log(0, "1", "\nOnDataReceived: Socket has been closed\n");
+            }
+            catch (SocketException se)
+            {
+                MessageBox.Show(se.Message);
+            }
+        }	
+
+        // Gets the machine IP (unused)
+        String GetIP()
+        {
+            String strHostName = Dns.GetHostName();
+
+            // Find host by name
+            IPHostEntry iphostentry = Dns.GetHostEntry(strHostName);
+            //IPHostEntry iphostentry = Dns.GetHostByName(strHostName);
+
+            // Grab the first IP addresses
+            String IPStr = "";
+            foreach (IPAddress ipaddress in iphostentry.AddressList)
+            {
+                IPStr = ipaddress.ToString();
+                return IPStr;
+            }
+            return IPStr;
+        }
+
+        // Grays out shit yo
+        private void UpdateControls(bool connected)
+        {
+            connectButton.Enabled = !connected;
+            disconnectButton.Enabled = connected;
+            string connectStatus = connected ? "Connected" : "Not Connected";
+            connectedStatus.Text = connectStatus;
+        }
+
+        public class SocketPacket
+        {
+            public System.Net.Sockets.Socket thisSocket;
+            public byte[] dataBuffer = new byte[1];
+        }
+    }
+}
